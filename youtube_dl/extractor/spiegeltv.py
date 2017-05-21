bd9577373b228efaf1abaa1c49ced1dd0bf20f4c
pajowu <git@ca.pajowu.de>
2017-05-21 23:57:54 +0300

    [spiegeltv] Update downloader for nexx cloud

--- youtube_dl/extractor/spiegeltv.py
+++ youtube_dl/extractor/spiegeltv.py
@@ -2,113 +2,70 @@
 from __future__ import unicode_literals
 
 from .common import InfoExtractor
-from ..compat import compat_urllib_parse_urlparse
+from ..compat import (compat_urllib_parse_urlparse, compat_urllib_parse_urlencode)
 from ..utils import (
     determine_ext,
     float_or_none,
 )
-
+import hashlib
+import re
 
 class SpiegeltvIE(InfoExtractor):
-    _VALID_URL = r'https?://(?:www\.)?spiegel\.tv/(?:#/)?filme/(?P<id>[\-a-z0-9]+)'
+    _VALID_URL = r'https?://(?:www\.)?spiegel\.tv/(?:#/)?videos/(?P<id>[0-9]+)([\-a-z0-9]+)'
     _TESTS = [{
-        'url': 'http://www.spiegel.tv/filme/flug-mh370/',
+        'url': 'http://www.spiegel.tv/videos/199901-wenn-teenager-muetter-werden',
         'info_dict': {
-            'id': 'flug-mh370',
-            'ext': 'm4v',
-            'title': 'Flug MH370',
-            'description': 'Das Rätsel um die Boeing 777 der Malaysia-Airlines',
-            'thumbnail': r're:http://.*\.jpg$',
-        },
-        'params': {
-            # m3u8 download
-            'skip_download': True,
+            'title': 'Wenn Teenager Mütter werden',
+            'subtitle': 'Kindeskinder',
+            'description': 'Im Haus Regenbogen leben in der Nähe von Flensburg 13 junge Mütter. Die Teenager kommen fast alle aus zerrütteten Verhältnissen. Für viele ist die frühe Mutterschaft eine Art Flucht aus ihrem vorherigen Leben. Sie träumen von einer eigenen heilen Familie, wollen ihren Kindern die Geborgenheit geben, die sie nie hatten. Doch in den wenigsten Fällen erfüllt sich der Wunsch. Eine SPIEGEL-TV-Reportage von Sanja Hardinghaus.',
+            'duration': 2689,
+            'id': '199901',
+            'ext': 'mp4'
         }
-    }, {
-        'url': 'http://www.spiegel.tv/#/filme/alleskino-die-wahrheit-ueber-maenner/',
-        'only_matching': True,
     }]
 
     def _real_extract(self, url):
         if '/#/' in url:
             url = url.replace('/#/', '/')
         video_id = self._match_id(url)
+
         webpage = self._download_webpage(url, video_id)
-        title = self._html_search_regex(r'<h1.*?>(.*?)</h1>', webpage, 'title')
+        domain_id = self._html_search_regex(r'http://require.nexx.cloud/([0-9]+)/one', webpage, 'domain_id')
+        domain_secret = self._html_search_regex(r'"hash":"([A-Z0-9]+)"', webpage, 'domain_secret')
+        device_id = self._html_search_regex(r'"nxp_devh":"([^"]+)"', webpage, 'device_id')
 
-        apihost = 'http://spiegeltv-ivms2-restapi.s3.amazonaws.com'
-        version_json = self._download_json(
-            '%s/version.json' % apihost, video_id,
-            note='Downloading version information')
-        version_name = version_json['version_name']
+        client_data_json_url = "https://api.nexx.cloud/v3/{did}/session/init".format(did=domain_id)
+        data = compat_urllib_parse_urlencode({"nxp_devh":device_id}).encode()
 
-        slug_json = self._download_json(
-            '%s/%s/restapi/slugs/%s.json' % (apihost, version_name, video_id),
-            video_id,
-            note='Downloading object information')
-        oid = slug_json['object_id']
+        client_data = self._download_json(
+            client_data_json_url, video_id,
+            note='Downloading client information',
+            data=data)
 
-        media_json = self._download_json(
-            '%s/%s/restapi/media/%s.json' % (apihost, version_name, oid),
-            video_id, note='Downloading media information')
-        uuid = media_json['uuid']
-        is_wide = media_json['is_wide']
+        client_id = client_data["result"]["general"]["cid"]
 
-        server_json = self._download_json(
-            'http://spiegeltv-prod-static.s3.amazonaws.com/projectConfigs/projectConfig.json',
-            video_id, note='Downloading server information')
+        request_token = hashlib.md5("byid".encode()+domain_id.encode()+domain_secret.encode()).hexdigest()
+        request_headers = {"X-Request-CID":client_id,"X-Request-Token":request_token}
 
-        format = '16x9' if is_wide else '4x3'
+        meta_json_url = "https://api.nexx.cloud/v3/{did}/videos/byid/{vid}".format(did=domain_id,vid=video_id)
+        request_data = {"additionalfields":"subtitle,description","addStreamDetails":"1"}
+        metadata = self._download_json(
+            meta_json_url, video_id,
+            note='Downloading metadata json', headers=request_headers,data=compat_urllib_parse_urlencode(request_data).encode())
+        duration_split = metadata["result"]["general"]["runtime"].split(":")
+        duration = int(duration_split[0])*60*60 + int(duration_split[1])*60 + int(duration_split[2])
 
-        formats = []
-        for streamingserver in server_json['streamingserver']:
-            endpoint = streamingserver.get('endpoint')
-            if not endpoint:
-                continue
-            play_path = 'mp4:%s_spiegeltv_0500_%s.m4v' % (uuid, format)
-            if endpoint.startswith('rtmp'):
-                formats.append({
-                    'url': endpoint,
-                    'format_id': 'rtmp',
-                    'app': compat_urllib_parse_urlparse(endpoint).path[1:],
-                    'play_path': play_path,
-                    'player_path': 'http://prod-static.spiegel.tv/frontend-076.swf',
-                    'ext': 'flv',
-                    'rtmp_live': True,
-                })
-            elif determine_ext(endpoint) == 'm3u8':
-                formats.append({
-                    'url': endpoint.replace('[video]', play_path),
-                    'ext': 'm4v',
-                    'format_id': 'hls',  # Prefer hls since it allows to workaround georestriction
-                    'protocol': 'm3u8',
-                    'preference': 1,
-                    'http_headers': {
-                        'Accept-Encoding': 'deflate',  # gzip causes trouble on the server side
-                    },
-                })
-            else:
-                formats.append({
-                    'url': endpoint,
-                })
-        self._check_formats(formats, video_id)
-
-        thumbnails = []
-        for image in media_json['images']:
-            thumbnails.append({
-                'url': image['url'],
-                'width': image['width'],
-                'height': image['height'],
-            })
-
-        description = media_json['subtitle']
-        duration = float_or_none(media_json.get('duration_in_ms'), scale=1000)
-
+        streamdata = metadata["result"]["streamdata"]
+        doc = "http://{cdn_host}/{cdn_vid}/{vid}_src.ism/Manifest(format=mpd-time-csf)".format(
+            cdn_host=streamdata["cdnShieldHTTP"], cdn_vid=streamdata["azureLocator"], vid=video_id)
+        formats = self._extract_mpd_formats(doc, video_id)
+        self._sort_formats(formats)
         return {
-            'id': video_id,
-            'title': title,
-            'description': description,
-            'duration': duration,
-            'thumbnails': thumbnails,
-            'formats': formats,
+            "id":video_id,
+            "title":metadata["result"]["general"]["title"],
+            "subtitle":metadata["result"]["general"]["subtitle"],
+            "description":metadata["result"]["general"]["description"],
+            "duration":duration,
+            "formats":formats,
+            "thumbnails":[{"url":metadata["result"]["imagedata"]["thumb"]}]
         }
